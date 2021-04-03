6c9bd3c9a772aa98214cb21276db659cccb93ce8
Marin Sablić <msablic@inceptum.hr>
2021-04-03 19:13:00 +0200

    moved upload function to main program

--- handler.js
+++ handler.js
@@ -38,47 +38,46 @@ $(document).ready(function() {
 
         uploadData(files)
     }
-
-    function uploadData(files) {
-
-        let url = 'https://cdn.zapwp.net';
-        let key = '51f0429e31298bf7461bed0a35589cb68d1babce';
-        let retina = 'true';
-        let webp = 'true';
-        let width = '1';
-        let quality = $('input[name="quality"]:checked').val();
-
-        let form_data = new FormData();
-
-        for (let index = 0; index < files.length; index++) {
-            form_data.append("files[]", files[index]);
-        }
-
-        // AJAX request
-        $.ajax({
-            url: 'upload.php',
-            type: 'post',
-            data: form_data,
-            dataType: 'json',
-            contentType: false,
-            processData: false,
-            success: function (response) {
-
-                for (var index = 0; index < response.length; index++) {
-                    var src = response[index];
-
-                    // Add img element in <div id='preview'>
-                    $('#preview').append('<img src="' + url +
-                        '/key:' + key +
-                        '/q:' + quality +
-                        '/retina:' + retina +
-                        '/webp:' + webp +
-                        '/w:' + width +
-                        '/url:' + src +
-                        '" width="200px;" height="200px">');
-                }
-            }
-        });
-    };
-
 });
+
+function uploadData(files) {
+
+    let url = 'https://cdn.zapwp.net';
+    let key = '51f0429e31298bf7461bed0a35589cb68d1babce';
+    let retina = 'true';
+    let webp = 'true';
+    let width = '1';
+    let quality = $('input[name="quality"]:checked').val();
+
+    let form_data = new FormData();
+
+    for (let index = 0; index < files.length; index++) {
+        form_data.append("files[]", files[index]);
+    }
+
+    // AJAX request
+    $.ajax({
+        url: 'upload.php',
+        type: 'post',
+        data: form_data,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        success: function (response) {
+
+            for (var index = 0; index < response.length; index++) {
+                var src = response[index];
+
+                // Add img element in <div id='preview'>
+                $('#preview').append('<img src="' + url +
+                    '/key:' + key +
+                    '/q:' + quality +
+                    '/retina:' + retina +
+                    '/webp:' + webp +
+                    '/w:' + width +
+                    '/url:' + src +
+                    '" width="200px;" height="200px">');
+            }
+        }
+    });
+};
